d6f47f1948b468d19de907a32221dfd9841f1845
Dylan Best <best.dylan15@gmail.com>
2020-08-27 19:46:00 -0400

    finished inquirer prompt to create employee array

--- app.js
+++ app.js
@@ -9,10 +9,83 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { push } = require("../../In Class Activities/Week-10/24-Mini-Project/Solved/lib/words");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+const employeeArr = [];
+let isManager = true;
+
+function createEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "Is this employee an Engineer or an Intern?",
+            choices: [
+                "Engineer",
+                "Intern",
+            ],
+            when: isManager === false
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the manager's name.",
+            when: isManager === true
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the employee's name.",
+            when: isManager === false
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter their employee ID."
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter their email address."
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Because this is the manager, enter their office number.",
+            when: isManager === true
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Because this is an Engineer, enter their GitHub username.",
+            when: (answers) => answers.type === "Engineer"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Because this is an Intern, enter their school.",
+            when: (answers) => answers.type === "Intern"
+        },
+        {
+            type: "confirm",
+            name: "continue",
+            message: "Do you want to add another employee?"
+        }
+    ]).then((answers) => {
+        employeeArr.push(answers);
+
+        isManager = false;
+        if(answers.continue) {
+            createEmployee();
+        }
+    });
+}
+
+createEmployee();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
